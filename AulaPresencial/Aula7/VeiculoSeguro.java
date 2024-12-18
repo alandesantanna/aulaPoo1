7cdb4205ab08e18d63af909a3b94ba20208faf57
Alan de Sant'Anna <108240669+alandesantanna@users.noreply.github.com>
2024-12-18 01:29:36 -0300

    18/12 01:29
    18/12 01:29

--- AulaPresencial/Aula7/VeiculoSeguro.java
+++
@@ -1,9 +0,0 @@
-package AulaPresencial.Aula7;
-
-public interface VeiculoSeguro {
-    public void acelerar();
-    public void desacelerar();
-    public void frear();
-    public void virarDireita();
-    public void virarEsquerda();
-}
